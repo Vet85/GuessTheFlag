6b7597bdd8ebba309fc6c27a5ed2c556314af754
Novichenko Vitaliy <novichenkovitaliy@gmail.com>
2023-08-18 17:47:06 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vitaliy Novichenko on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
